21c15f26cd5c6cab5334df091f09de1e72661058
emran younas <126257910+emrantims@users.noreply.github.com>
2026-05-20 16:08:52 +0400

    Refine app shell layout spacing

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -7,10 +7,12 @@ export function DashboardLayout() {
   return (
     <div className="flex h-screen bg-background font-sans text-primary overflow-hidden">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden lg:ml-[280px]">
+      <div className="flex-1 flex flex-col overflow-hidden lg:ml-[248px]">
         <Header />
-        <main className="flex-1 p-8 overflow-y-auto flex flex-col gap-6 relative">
-          <Outlet />
+        <main className="custom-scrollbar flex-1 overflow-y-auto p-5 md:p-7">
+          <div className="mx-auto flex w-full max-w-[1480px] flex-col gap-6">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
